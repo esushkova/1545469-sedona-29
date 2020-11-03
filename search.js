afa83f06ef1e12d1c4619c5f4a1fe277d50a631c
esushkova <e.isxakova@gmail.com>
2020-11-03 22:54:51 +0300

    js для закрытия формы

--- search.js
+++ search.js
@@ -2,8 +2,9 @@ const searchButton = document.querySelector(".search_button");
 const popUp = document.querySelector(".popup");
 const searchForm = popUp.querySelector(".search-form");
 
+popUp.classList.add("popup-hide");
+
 searchButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     popUp.classList.toggle("popup-hide");
 });
-

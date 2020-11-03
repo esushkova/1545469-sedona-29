5f3704ae20f1cab240bcbbe3e6257541116f4979
esushkova <e.isxakova@gmail.com>
2020-11-03 21:25:27 +0300

    Правки стилей ссостояний

--- search.js
+++ search.js
@@ -1,7 +1,6 @@
 const searchButton = document.querySelector(".search_button");
 const popUp = document.querySelector(".popup");
 const searchForm = popUp.querySelector(".search-form");
-const checkIn = popUp.querySelector(".modal-check-in");
 
 searchButton.addEventListener("click", function (evt) {
     evt.preventDefault();
